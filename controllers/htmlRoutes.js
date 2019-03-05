6c51621b7eb6596fef0a5e18532b1c43162fef45
Jenina Lanzi <jeninalanzi@gmail.com>
2019-03-05 15:07:33 -0600

    Got routes working so far and handlebars displaying

---
+++ controllers/htmlRoutes.js
@@ -0,0 +1,112 @@
+// Scraping tools and express dependency
+var axios = require("axios");
+var cheerio = require("cheerio");
+var express = require("express");
+
+var app = express.Router();
+
+// Import the models to use their database functions.
+var db = require("../models");
+
+// GET route for scraping the NuclearBlast news website
+app.get("/scrape", function(req, res) {
+    // Grab the body of html with axios
+    axios.get("https://www.nuclearblast.com/en/music/news/index.html")
+    .then(function(response) {
+        // Load the body into cheerio, save into a short selector
+        var $ = cheerio.load(response.data);
+
+        // Grab elements from the DOM that contains articles
+        $("td.nth-1").each(function(i, element) {
+            var result = {};
+
+            // Pass the scraped values into result
+            result.headline = $(this).children("h2").children("a").text();
+            result.link = $(this).children("h2").children("a").attr("href");
+            result.summary = $(this).children("p.no-margin").text();
+
+            // Create a new Article using the "result" of scraping
+            db.Article.create(result)
+            .then(function(dbArticle) {
+                // View the results in the console.
+                console.log("RESULT OF DB ARTICLE");
+                console.log(dbArticle);
+            })
+            .catch(function(err) {
+                // If error occurs, log it.
+                console.log(err);
+            });
+        });
+        
+        // Send a message to the client
+        res.send("Scrape complete. <a href='/'>Home Page</a>")
+    });
+});
+
+// Route to delete notes
+app.delete("/articles/:id", function(req, res) {
+    console.log("id: " + req.params.id);
+    // Find Article by ID and remove it
+    db.Article.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.json({ message: "Note Deleted!" });
+        }
+    });
+});
+
+// Route to get all Articles from the DB
+app.get("/", function(req, res) {
+    // Grab every document in Articles collection
+    db.Article.find({})
+    .then(function(data) {
+        var hbsObject = {
+            scrapes: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    })
+    .catch(function(err) {
+        // If error occurs notify the client.
+        res.json(err);
+    });
+});
+
+// 2nd Route for getting all Articles from DB
+app.get("/articles", function(req, res) {
+    // Grab every document from Articles
+    db.Article.find({})
+    .then(function(dbArticle) {
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by ID
+// Then populate all Comments associated with it
+app.get("/articles/:id", function(req, res) {
+    //Populate all Comments associated with Article
+    db.Article.findOne({_id: req.params.id})
+    .populate("comment")
+    .then(function(dbArticle) {
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+
+// Route for saving & updating an Article's associated Comment(s)
+app.post("/articles/:id", function(req, res) {
+    // Create a new Note and pass the req.body as the entry
+    db.Note.create(req.body)
+})
+
+
+
+// Export for other apps to use.
+module.exports = app;
--- server.js
+++ server.js
@@ -1,95 +1,35 @@
 // Dependencies
-const express = require("express");
-const exphbs = require("express-handlebars");
-const mongoose = require("mongoose");
-
-// Scraping tools
-const axios = require("axios");
-const cheerio = require("cheerio");
-
-// Require all models
-const db = require("./models");
+var express = require("express");
+var mongoose = require("mongoose");
 
 // Set port
-const PORT = 3000;
+var PORT = 3000;
 
-// Initialize Express
-const app = express();
+// Initialize express
+var app = express();
+
+// Use HTML routes
+var routes = require("./controllers/htmlRoutes.js");
 
 // Configure middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Set public as the static folder
+
+// Serve static content from the public directory.
 app.use(express.static("public"));
-// Set handlebars as view engine
+
+// Set handlebars.
+var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// If deployed, use the deployed database.
-// Otherwise use the local mongoHeadlines database
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// Set app to use html routes.
+app.use(routes);
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
-// ===================================================================
-// ============================ ROUTES ===============================
-
-// The GET route for scraping the MetalSucks News website
-app.get("/scrape", function(req, res) {
-    // Grab the html body with axios
-    axios.get("https://www.nuclearblast.com/en/music/news/index.html")
-    .then(function(response) {
-        // Load the body into cheerio, save into a short selector
-        const $ = cheerio.load(response.data);
-
-        $("td.nth-1").each(function(i, element) {
-            var result = {};
-
-            // Pass the scraped values into result
-            result.headline = $(this).children("h2").children("a").text();
-            result.link = $(this).children("h2").children("a").attr("href");
-            result.summary = $(this).children("p.no-margin").text();
-
-            // SUCCESSFUL - this does print results to console
-            console.log(result.headline);
-            console.log(result.summary);
-            console.log("---------------------------");
-            
-            // Create a new Article using the "result" of scraping
-            db.Article.create(result).then(function(dbArticle) {
-                console.log("RESULT OF DB ARTICLE");
-                console.log(dbArticle);
-            })
-            .catch(function(err) {
-                console.log(err);
-            });
-        });
-
-        // Route for getting all the Articles from db
-        // WARNING: THIS CODE NEEDS EDITING
-        // DOES NOT WORK YET - SERVER DOESN'T COMMUNICATE
-        // TO THIS
-        app.get("/articles", function(req, res) {
-            console.log("this works");
-            // Grab all articles & display as JSON
-            db.Article.find({})
-                .then(function(dbArticle) {
-                    res.json(dbArticle);
-                })
-                .catch(function(err) {
-                    res.json(err);
-                });
-        });
-
-
-
-        
-    });
-});
-
-
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost/mongoheadline", { useNewUrlParser: true });
 
 // Start the server
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT);
+    console.log("App running on port " + PORT + "!");
 });
